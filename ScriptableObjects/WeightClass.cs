8e74fe8bb15f79d61a0f27205157e59f9cfbe0a8
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-11-15 20:00:15 +0200

    Create WeightClass.cs

---
+++ ScriptableObjects/WeightClass.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeightClass", menuName = "Weight Class ")]
+public class WeightClass : ScriptableObject
+{
+    public float speed;
+    public int NAdmg;
+    public int HAdmg;
+    public int RAdmg;
+    public int MAdmg;
+    public int Sdmg;
+
+}
