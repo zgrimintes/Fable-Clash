7483ca56f43096d5c8ceb568b78ece8f0787bacf
Tibirean Petru <peptib07@gmail.com>
2023-11-16 21:10:21 +0200

    Improved jumping mechanic

--- Scripts/PlayerManager.cs
+++ Scripts/PlayerManager.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] private float gravityS = 1;
-    [SerializeField] private float fallGravityS = 4;
+    [SerializeField] private float gravityScale = 1;
+    [SerializeField] private float fallGravityScale = 4;
+    [SerializeField] private LayerMask layer;
 
     public float speed;
     public float jumpForce;
     public Rigidbody2D rb;
+    private BoxCollider2D coll;
     private bool isGrounded; //To check if the player touches the ground
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -23,39 +26,23 @@ public class PlayerManager : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
 
-        Vector2 movement = new Vector2(x, rb.velocity.y);
+        Vector2 movement = new Vector2(x * speed, rb.velocity.y);
 
-        rb.velocity = movement * speed;
+        rb.velocity = movement;
     }
 
     private void Update()
     {
-        /// Doesn't work!!! isGrounded = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, 1);
+        isGrounded = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, layer);
 
-        if (isGrounded == false && Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("jump");
-            rb.AddForce( new Vector2(rb.velocity.y, jumpForce));
+            rb.velocity = new Vector2(rb.velocity.y, jumpForce);
         }
 
-        if (rb.velocity.y > 0) rb.gravityScale = gravityS;
-        else rb.gravityScale = fallGravityS;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
+        if (rb.velocity.y > 0) rb.gravityScale = gravityScale;
+        else rb.gravityScale = fallGravityScale;
     }
 
 }
