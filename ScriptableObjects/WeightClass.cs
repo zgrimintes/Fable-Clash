ad1d121df2bf0c148318adbc09cd50167ed348c3
Tibirean Petru <peptib07@gmail.com>
2023-11-18 13:30:20 +0200

    Name Update + new variable

--- ScriptableObjects/WeightClass.cs
+++ ScriptableObjects/WeightClass.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeightClass", menuName = "Weight Class ")]
 public class WeightClass : ScriptableObject
 {
+    public string Class_Type;
     public float speed;
-    public int NAdmg;
-    public int HAdmg;
-    public int RAdmg;
-    public int MAdmg;
-    public int Sdmg;
+    public int _NA_dmg;
+    public int _HA_dmg;
+    public int _RA_dmg;
+    public int _MA_dmg;
+    public int _S_dmg;
 
 }
