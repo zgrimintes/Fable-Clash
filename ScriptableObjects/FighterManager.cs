b8592f7a2466febcdc54669e0ba6129869a7326e
Tibirean Petru <peptib07@gmail.com>
2023-11-18 16:27:24 +0200

    Update

--- ScriptableObjects/FighterManager.cs
+++ ScriptableObjects/FighterManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "FighterManager", menuName = "Figher Manager")]
@@ -9,4 +10,10 @@ public class FighterManager : ScriptableObject
     public int mana;
     public ScriptableObject w_Class;
     public string characterName;
+
+    public void normal_Attack(GameObject parent)
+    {
+        mana -= 1;
+        parent.GetComponent<PlayerManager>().mana = mana;
+    }
 }
