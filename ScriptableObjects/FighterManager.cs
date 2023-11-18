37f374766f2268710d5453765076e8aa0de48e80
Tibirean Petru <peptib07@gmail.com>
2023-11-18 13:29:40 +0200

    Name Update

--- ScriptableObjects/FighterManager.cs
+++ ScriptableObjects/FighterManager.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu( fileName = "FighterManager", menuName ="Figher Manager")]
+[CreateAssetMenu(fileName = "FighterManager", menuName = "Figher Manager")]
 public class FighterManager : ScriptableObject
 {
     public int HP;
-    public ScriptableObject wClass;
+    public ScriptableObject w_Class;
     public string characterName;
 }
