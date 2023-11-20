f1d752b5e81dc1a503c9b0d1db783c8ec8324c17
Tibirean Petru <peptib07@gmail.com>
2023-11-20 22:10:04 +0200

    Change script in order to have more uses

--- Scripts/FollowPlayer.cs
+++ Scripts/FollowPlayer.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject toFollow;
+    public float offsetZ;
 
     void Update()
     {
-        gameObject.transform.position = new Vector3(toFollow.transform.position.x, toFollow.transform.position.y, -10);
+        gameObject.transform.position = new Vector3(toFollow.transform.position.x, toFollow.transform.position.y, toFollow.transform.position.z + offsetZ);
     }
 }
