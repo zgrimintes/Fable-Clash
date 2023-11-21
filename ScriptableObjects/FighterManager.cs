866d93e058813dbb0e88726091c70afe83aa6f52
Tibirean Petru <peptib07@gmail.com>
2023-11-21 19:06:39 +0200

    Added mana gain

--- ScriptableObjects/FighterManager.cs
+++ ScriptableObjects/FighterManager.cs
@@ -8,12 +8,20 @@ public class FighterManager : ScriptableObject
 {
     public int HP;
     public int mana;
+    public int stamina;
     public ScriptableObject w_Class;
     public string characterName;
 
     public void normal_Attack(GameObject parent)
     {
         mana -= 1;
+        parent.GetComponent<PlayerManager>().mana = mana;
+        parent.GetComponent<AttackManager>().Attack();
+    }
+
+    public void mana_Gain(GameObject parent)
+    {
+        mana += 1;
         parent.GetComponent<PlayerManager>().mana = mana;
     }
 }
