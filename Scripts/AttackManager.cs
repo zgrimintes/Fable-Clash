5d6e4e35e6b121c9b97325531c570d1536eae6a3
Tibirean Petru <peptib07@gmail.com>
2023-11-22 18:43:16 +0200

    New Function

--- Scripts/AttackManager.cs
+++ Scripts/AttackManager.cs
@@ -10,20 +10,26 @@ public class AttackManager : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayer;
+    GameObject wp;
 
-    public void Attack()
+    public void normal_Attack()
     {
-        GameObject wp = Instantiate(Weapon, transform.position, Quaternion.identity);
+        wp = Instantiate(Weapon, transform.position, Quaternion.identity);
         if (GetComponent<PlayerManager>().horizontalS == -1) wp.GetComponent<Animator>().Play("SwordSwingLeft");
         else if (GetComponent<PlayerManager>().horizontalS == 1) wp.GetComponent<Animator>().Play("SwordSwing");
 
-        wp.AddComponent<FollowPlayer>().toFollow = gameObject; //Adding the FollowPlayer script 
+        wp.AddComponent<FollowPlayer>().toFollow = gameObject; //Adding the FollowPlayer
 
+        checkForColls();
+    }
+
+    public void checkForColls()
+    {
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
 
         foreach (Collider2D enemy in hitEnemies)
         {
-            Debug.Log("Hit " + enemy.name);
+            enemy.GetComponent<EnemyController>().take_damage(1);
         }
     }
 }
