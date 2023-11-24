06acb91a33b273dafc99840ff2f188649eee9f1d
Tibirean Petru <peptib07@gmail.com>
2023-11-24 12:51:55 +0200

    Regenerate stamina

--- Scripts/StatsRegen.cs
+++ Scripts/StatsRegen.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class StatsRegen : MonoBehaviour
 {
     [SerializeField] private float timeToGainMana = 15f;
+    [SerializeField] private float timeToGainStamina = 3f;
     [SerializeField] private FighterManager fighterManager;
     private float lastManaGain;
+    private float lastStaminaGain;
     private int mana;
+    private int stamina;
 
 
     void Update()
     {
         mana = GetComponent<CharacterManager>().mana;
+        stamina = GetComponent<CharacterManager>().stamina;
 
-        if (mana == 4) return;
+        if (mana == 4 && stamina == 5) return;
 
-        if (Time.time - lastManaGain > timeToGainMana)
+        if (mana != 4)
         {
-            lastManaGain = Time.time;
-            fighterManager.mana_Gain(gameObject);
-            GetComponent<CharacterManager>().updateText();
+            if (Time.time - lastManaGain > timeToGainMana)
+            {
+                lastManaGain = Time.time;
+                fighterManager.mana_Gain(gameObject);
+                GetComponent<CharacterManager>().updateText();
+            }
+        }
+
+        if (stamina != 5)
+        {
+            if (Time.time - lastStaminaGain > timeToGainStamina)
+            {
+                lastStaminaGain = Time.time;
+                fighterManager.stamina_Gain(gameObject);
+                GetComponent<CharacterManager>().updateText();
+            }
         }
     }
 }
