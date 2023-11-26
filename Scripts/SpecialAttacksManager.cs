cf09428f6e11f80c3720c0158a09a4f92d134f67
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-11-26 22:22:12 +0200

    Praslea's SA works

--- Scripts/SpecialAttacksManager.cs
+++ Scripts/SpecialAttacksManager.cs
@@ -11,29 +11,29 @@ public class SpecialAttacksManager : MonoBehaviour
     float[] rainStartPoints = new float[7];
 
     int hits_Praslea = 0;
+    int index;
 
     private void Start()
     {
         attackManager = GetComponent<AttackManager>();
     }
 
-    public int Praslea_SA(GameObject wp, GameObject projectile)
+    public void Praslea_SA(GameObject wp, GameObject projectile)
     {
+        hits_Praslea = 0;
         wp = Instantiate(projectile, transform.position, Quaternion.identity);
         StartCoroutine(fly(wp));
 
-        for (int index = 0; index < 7; index++)
+        for (index = 0; index < 7; index++) 
         {
-            rainStartPoints[index] = Random.Range(enemy.transform.position.x - 7f, enemy.transform.position.x + 7f); //Spaw in proximity of the enemy
-            projectiles[index] = Instantiate(projectile, new Vector3(rainStartPoints[index], 15, 0), Quaternion.Euler(0, 0, 180));
+            rainStartPoints[index] = Random.Range(enemy.transform.position.x - 6f, enemy.transform.position.x + 6f); //Spaw in proximity of the enemy
+            projectiles[index] = Instantiate(projectile, new Vector3(rainStartPoints[index], 15, 0), Quaternion.Euler(0, 0, 180)); 
         }
 
-        for (int index = 0; index < 7; index++)
+        for (index = 0; index < 7; index++)
         {
             StartCoroutine(fall(projectiles[index]));
         }
-
-        return hits_Praslea;
     }
 
     public IEnumerator fly(GameObject wp)
@@ -49,14 +49,19 @@ public class SpecialAttacksManager : MonoBehaviour
 
     public IEnumerator fall(GameObject wp)
     {
-        while (wp.transform.position.y > -2f && Physics2D.OverlapBox(wp.transform.position, new Vector2(0.3f, 0.3f), 0, layer) == null)
+        while (wp.transform.position.y > -2f)
         {
+            if (Physics2D.OverlapBox(wp.transform.position, new Vector2(0.3f, 0.3f), 0, layer) != null)
+            {
+                hits_Praslea++;
+                break;
+            }
+
             wp.transform.position = wp.transform.position - new Vector3(0, 0.05f, 0);
             yield return null;
         }
 
-        if (Physics2D.OverlapBox(wp.transform.position, new Vector2(0.3f, 0.3f), 0, layer) != null) hits_Praslea++;
-
+        GetComponent<CharacterManager>().enemy.GetComponent<CharacterManager>().take_damage(hits_Praslea); hits_Praslea = 0;
         Destroy(wp);
     }
 }
