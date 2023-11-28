8a7b02d91f1b64cf70d3526960e1d7eed74f68d3
Tibirean Petru <peptib07@gmail.com>
2023-11-28 19:13:40 +0200

    Logic for the wait state

--- Enemy/WaitState.cs
+++ Enemy/WaitState.cs
@@ -1,1 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitState : EnemyState
+{
+    protected float timeSinceStopped;
+
+    public WaitState(EnemyController enemy, EnemyStateMachine stateMachine) : base(enemy, stateMachine)
+    {
+    }
+
+    public override void EnterState()
+    {
+        base.EnterState();
+        timeSinceStopped = Time.time;
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+    }
+
+    public override void FrameUpdate()
+    {
+        base.FrameUpdate();
+
+        if (Time.time - timeSinceStopped > 2f)
+        {
+            timeSinceStopped = Time.time;
+            enemy.GetComponent<CharacterManager>().try_RA();
+
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
 
