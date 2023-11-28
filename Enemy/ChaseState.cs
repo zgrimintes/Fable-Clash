f9f2e5d15e744f42cd1b29ecccf051eea46e6dbd
Tibirean Petru <peptib07@gmail.com>
2023-11-28 19:13:16 +0200

    Logic for the chase state

--- Enemy/ChaseState.cs
+++ Enemy/ChaseState.cs
@@ -1,1 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ChaseState : EnemyState
+{
+    private GameObject playerEnemy;
+
+    public ChaseState(EnemyController enemy, EnemyStateMachine stateMachine) : base(enemy, stateMachine)
+    {
+    }
+
+    public override void EnterState()
+    {
+        base.EnterState();
+
+        playerEnemy = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+    }
+
+    public override void FrameUpdate()
+    {
+        base.FrameUpdate();
+
+        if (!enemy.isTooFar)
+        {
+            enemy.MoveEnemy(Vector2.zero);
+            enemy.stateMachine.Change(enemy.waitState);
+            return;
+        }
+
+        enemy.MoveEnemy(new Vector2(enemy.playerInstance.transform.position.x - enemy.transform.position.x, 0).normalized * (enemy.speed / 2));
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+}
 
