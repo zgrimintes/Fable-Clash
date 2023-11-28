2aed5b7176b16cd2c43f452f9e176ae73b2c05b8
Tibirean Petru <peptib07@gmail.com>
2023-11-28 19:12:20 +0200

    State Machine manager

--- Enemy/EnemyStateMachine.cs
+++ Enemy/EnemyStateMachine.cs
@@ -1,1 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateMachine
+{
+    public EnemyState CurrentEnemyState;
+
+    public void Initialize(EnemyState enemyState)
+    {
+        CurrentEnemyState = enemyState;
+        CurrentEnemyState.EnterState();
+    }
+
+    public void Change(EnemyState enemyState)
+    {
+        CurrentEnemyState.ExitState();
+        CurrentEnemyState = enemyState;
+        CurrentEnemyState.EnterState();
+    }
+}
 
