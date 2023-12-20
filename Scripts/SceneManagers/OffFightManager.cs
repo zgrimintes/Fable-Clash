84701723522cd7c2e90e601f1dc98d2734fec778
Tibirean Petru <peptib07@gmail.com>
2023-12-20 14:15:25 +0200

    Added logic for end of round

--- Scripts/SceneManagers/OffFightManager.cs
+++ Scripts/SceneManagers/OffFightManager.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OffFinghtManager : MonoBehaviour
 {
-    public TextMeshProUGUI textMeshProUGUI;
+    public TextMeshProUGUI countdown, scoreIndicator;
     public GameObject enemy, player;
 
     private void Awake()
     {
-        textMeshProUGUI = GetComponentInChildren<TextMeshProUGUI>();
-        textMeshProUGUI.text = "3";
+        initializeCanvas();
+
         enemy = GameObject.Find("Enemy");
         player = GameObject.Find("Player");
+    }
+
+    private void initializeCanvas()
+    {
+        TextMeshProUGUI[] children = GetComponentsInChildren<TextMeshProUGUI>();
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            switch (children[i].name)
+            {
+                case "CountdownText":
+                    countdown = children[i];
+                    countdown.text = "3";
+                    break;
+                case "ScoreIndicator":
+                    scoreIndicator = children[i];
+                    scoreIndicator.enabled = false;
+                    break;
+            }
+        }
     }
 
     private void minusOneCountdown(int n)
@@ -21,15 +43,48 @@ public class OffFinghtManager : MonoBehaviour
         switch (n)
         {
             case 3:
-                textMeshProUGUI.text = "2";
+                countdown.text = "2";
                 break;
             case 2:
-                textMeshProUGUI.text = "1";
+                countdown.text = "1";
                 break;
             case 1:
-                textMeshProUGUI.text = "Fight!";
+                countdown.text = "Fight!";
                 break;
         }
+    }
+
+    public async void roundWon()
+    {
+        GameObject enemy, player;
+        TextMeshProUGUI scoreIndicator;
+        enemy = GameObject.Find("Enemy"); enemy.GetComponent<EnemyController>().canAttack = false;
+        player = GameObject.Find("Player"); player.GetComponent<PlayerManager>().canMove = false;
+        gameObject.SetActive(true);
+        scoreIndicator = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        scoreIndicator.enabled = true;
+
+        int winsPlayer = player.GetComponent<CharacterManager>().fighterManager.roundsWon,
+            winsEnemy = enemy.GetComponent<CharacterManager>().fighterManager.roundsWon;
+
+        if (winsEnemy == 2)
+        {
+            scoreIndicator.text = enemy.GetComponent<CharacterManager>().fighterManager.name + " has won!";
+            return;
+        }
+        else if (winsPlayer == 2)
+        {
+            scoreIndicator.text = player.GetComponent<CharacterManager>().fighterManager.name + " has won!";
+            return;
+        }
+        else
+        {
+            scoreIndicator.text = winsPlayer + " - " + winsEnemy;
+        }
+
+        await Task.Delay(1500);
+
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void startFight()
@@ -37,5 +92,11 @@ public class OffFinghtManager : MonoBehaviour
         enemy.GetComponent<EnemyController>().waitState.StartFight();
         player.GetComponent<PlayerManager>().canMove = true;
         GetComponentInParent<Canvas>().gameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    public void startOfFight()
+    {
+
     }
 }
