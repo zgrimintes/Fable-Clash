93b4dcf286962d43137c5dbfe605be3917377483
Tibirean Petru <peptib07@gmail.com>
2023-12-20 18:59:11 +0200

    Added start function call

--- Scripts/SceneManagers/MainMenuManager.cs
+++ Scripts/SceneManagers/MainMenuManager.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void FreePlay() //Start the freeplay mode
+    public void FreePlay() //Start the free play mode
     {
         SceneManager.LoadScene("SampleScene");
+        StartCoroutine(WaitAFrame());
+    }
+
+    IEnumerator WaitAFrame()
+    {
+        yield return 0;
+
+        GameManager.Instance.updateGameState(GameStates.Start);
     }
 }
