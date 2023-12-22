85e76c7df02136da2c8aba121a4dc25b2f3b718f
Tibirean Petru <peptib07@gmail.com>
2023-12-22 10:27:15 +0200

    Added dashing to enemy

--- Scripts/Enemy/EnemyController.cs
+++ Scripts/Enemy/EnemyController.cs
@@ -11,6 +11,7 @@ public class EnemyController : CharacterManager
     public GameObject playerInstance;
     public bool isTooFar = true;
     public bool canAttack = false;
+    public bool isAbove = false;
 
     #region State Machine Variables
 
@@ -41,15 +42,25 @@ public class EnemyController : CharacterManager
 
         if (transform.position.x > playerInstance.transform.position.x && transform.localScale.x > 0) transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y);
         else if (transform.position.x < playerInstance.transform.position.x && transform.localScale.x < 0) transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y);
+
+        checkIfAbove();
     }
 
     public void MoveEnemy(Vector2 velocity)
     {
+        if (isDashing) return; //If dashing stop the wrinting of velocity
+
         if (!isKnockback) rb.velocity = velocity;//Stop writing the velocity if you are getting knockbacked
         else
         {
             rb.velocity = new Vector2(velocity.x * -5, velocity.y);
             StartCoroutine(Knockback());
         }
+    }
+
+    protected void checkIfAbove() //Check if the player is above so that can change states if so
+    {
+        if (Physics2D.BoxCast(transform.position + new Vector3(0, 3, 0), new Vector2(.2f, .2f), 0, Vector2.up, .1f, enemyLayer) && !isDashing)
+            StartCoroutine(Dashh());
     }
 }
