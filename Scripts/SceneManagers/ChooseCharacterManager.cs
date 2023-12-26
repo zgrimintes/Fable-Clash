b14ecc8d30689d769e23ce60a62cbf39018ca116
Tibirean Petru <peptib07@gmail.com>
2023-12-26 16:41:09 +0200

    Now starting the game calls startOfFight too

--- Scripts/SceneManagers/ChooseCharacterManager.cs
+++ Scripts/SceneManagers/ChooseCharacterManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ChoseCharacterManager : MonoBehaviour
@@ -7,6 +8,7 @@ public class ChoseCharacterManager : MonoBehaviour
     GameObject player, enemy;
 
     public FighterManager[] fighterManagers = new FighterManager[9];
+    public TextMeshProUGUI countdownText;
 
     private void Start()
     {
@@ -17,7 +19,7 @@ public class ChoseCharacterManager : MonoBehaviour
     public void characterChoosed(int c)
     {
         /* c is the parameter recievd from teh buttons to indicate witch character is selected
-         * first of all positive integers represent the player and ther negative pairs represent the enemy
+         * first of all positive integers represent the player and ther negative pairs represent the enemy, then we have:
          * 1 - Prislea, 2 - Harap-Alb, 3 - Greuceanul,
          * 4 - Spanul, 5 - Capcaunul, 6 - Zmeul,
          * 7 - Zgripturoaica, 8 - Balaurul, 9 - Crisnicul
@@ -38,5 +40,14 @@ public class ChoseCharacterManager : MonoBehaviour
                 enemy.GetComponent<CharacterManager>().fighterManager = fighterManagers[c - 1];
                 return;
         }
+    }
+
+    public void startGame()
+    {
+        if (player.GetComponent<CharacterManager>().fighterManager == null || enemy.GetComponent<CharacterManager>().fighterManager == null) return;
+
+        gameObject.SetActive(false);
+        countdownText.GetComponent<Animator>().Play("Countdown");
+        countdownText.GetComponentInParent<OffFinghtManager>().startOfFight();
     }
 }
