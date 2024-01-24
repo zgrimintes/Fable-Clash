f6f23e9ca36037619e6cd46c1ba26f7bd6be3d34
Tibirean Petru <peptib07@gmail.com>
2024-01-24 23:44:03 +0200

    Added buttons for each chapter

--- Scripts/SceneManagers/ChapterSelectManger.cs
+++ Scripts/SceneManagers/ChapterSelectManger.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChapterSelectManager : MonoBehaviour
 {
-    public static bool CH1 = true, CH2 = false, CH3 = false;
+    public static bool CH1 = true, CH2 = true, CH3 = false;
+    public GameObject buttonCH1, buttonCH2, buttonCH3;
+
+    public void Start()
+    {
+        buttonCH1.GetComponent<Button>().enabled = CH1;
+        buttonCH2.GetComponent<Button>().enabled = CH2;
+        buttonCH3.GetComponent<Button>().enabled = CH3;
+    }
 
     public void backToMainMenu()
     {
@@ -15,5 +24,21 @@ public class ChapterSelectManager : MonoBehaviour
     public void startChapetr1()
     {
         StoryTellingManager.story = true;
+        StoryTellingManager.nextStoryInstance = 0;
+        SceneManager.LoadScene("StoryTelling");
+    }
+
+    public void startChapetr2()
+    {
+        StoryTellingManager.story = true;
+        StoryTellingManager.nextStoryInstance = 44;
+        SceneManager.LoadScene("StoryTelling");
+    }
+
+    public void startChapetr3()
+    {
+        StoryTellingManager.story = true;
+        StoryTellingManager.nextStoryInstance = 0;
+        SceneManager.LoadScene("StoryTelling");
     }
 }
