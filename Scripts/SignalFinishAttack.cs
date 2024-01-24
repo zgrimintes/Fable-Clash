a6e78afcb776bf9257d5d91c681f13fa001af3e0
Tibirean Petru <peptib07@gmail.com>
2024-01-24 19:20:23 +0200

    Added signal; for the rest of the attaks

--- Scripts/SignalFinishAttack.cs
+++ Scripts/SignalFinishAttack.cs
@@ -7,5 +7,9 @@ public class SignalFinishAttack : MonoBehaviour
     public void signalFinishAttack()
     {
         GetComponent<CharacterManager>().animator.SetBool("NA", false);
+        GetComponent<CharacterManager>().animator.SetBool("HA", false);
+        GetComponent<CharacterManager>().animator.SetBool("RA", false);
+        GetComponent<CharacterManager>().animator.SetBool("SA", false);
+        GetComponent<CharacterManager>().animator.SetBool("Attacking", false);
     }
 }
