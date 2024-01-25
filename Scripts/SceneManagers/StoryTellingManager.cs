f0488a37d47bdba5274baba359d76c171f0a48e5
Tibirean Petru <peptib07@gmail.com>
2024-01-25 19:00:01 +0200

    Added chapter 2 and fixed some writing errors

--- Scripts/SceneManagers/StoryTellingManager.cs
+++ Scripts/SceneManagers/StoryTellingManager.cs
@@ -22,12 +22,16 @@ public class StoryTellingManager : MonoBehaviour
 
         if (startStoryText == null) return;
 
-        if (nextStoryInstance == 0 || nextStoryInstance == 44)
+        if (nextStoryInstance == 0 || nextStoryInstance == 44 || nextStoryInstance == 68)
         {
             if (nextStoryInstance == 0) startStoryText.SetActive(true);
             else if (nextStoryInstance == 44)
             {
-                storyText1.GetComponent<TextMeshProUGUI>().text = nextStory(43);
+                storyText1.GetComponent<TextMeshProUGUI>().text = "Chapter 2";
+            }
+            else if (nextStoryInstance == 68)
+            {
+                storyText1.GetComponent<TextMeshProUGUI>().text = "Chapter 3";
             }
         }
         else
@@ -96,15 +100,15 @@ public class StoryTellingManager : MonoBehaviour
             case 11:
                 storyText1.transform.localPosition = new Vector2(0, 100);
                 return "The words are heard across both realms, as if the heavens themselves were speaking them, and with no other option left, desperately, each realm chose three warriors to represent them.\r\n";
-            case 12: return "The realm of Men chose: Praslea, Harap-Alb and Greuceanu.\r\n\r\nThe realm of Beasts chose: Zmeul, Spanul, Capcaunul.\r\n";
-            case 13: return "The warriors all easily reached the peak and depth respectively, the hardships of nature were no match for them. When Harap-Alb had finally reached the peak, only moments after Praslea and Greuceanu, the same voice was heard from the heavens:";
+            case 12: return "The realm of Men chose: Prislea, Harap-Alb and Greuceanu.\r\n\r\nThe realm of Beasts chose: Zmeul, Spinul, Capcaunul.\r\n";
+            case 13: return "The warriors all easily reached the peak and depth respectively, the hardships of nature were no match for them. When Harap-Alb had finally reached the peak, only moments after Prislea and Greuceanu, the same voice was heard from the heavens:";
             case 14: return "\"Let the Tournament of the Golden Apple begin! The fights will be arranged in sets of three, each one of you facing one opponent! The first to win two of the three fights will be crowned the winner of the fight! Any and all tiebreakers will be decided with a final showdown. Now, let the fighting commence!\"";
             case 15:
                 storyText1.GetComponent<TextMeshProUGUI>().font = arhaicFont;
                 return "1st fight:";
             case 16:
                 storyText2.GetComponent<TextMeshProUGUI>().font = arhaicFont;
-                return "Praslea vs Zmeul\r\n";
+                return "Prislea vs Zmeul\r\n";
             case 17:
                 setFight(1);
                 return "";
@@ -140,11 +144,11 @@ public class StoryTellingManager : MonoBehaviour
             case 31:
                 storyText1.GetComponent<TextMeshProUGUI>().font = arhaicFont;
                 return "And with this final win, the Realm of Men won the tournament.";
-            case 32: return "\"Incredible! Delightful! With such a showing I am more than honored to present the Golden Apple to our winner, Praslea! \r\nCongratulations boy, you may now make your wish!\"";
-            case 33: return "Finally getting his hands on the Golden Apple, Praslea could feel a macabre aura emanating from it, but pushed it to the back of his head, in order to save his realm. \r\nWhile holding the Golden Apple above his head, Praslea shouted:\r\n";
+            case 32: return "\"Incredible! Delightful! With such a showing I am more than honored to present the Golden Apple to our winner, Prislea! \r\nCongratulations boy, you may now make your wish!\"";
+            case 33: return "Finally getting his hands on the Golden Apple, Prislea could feel a macabre aura emanating from it, but pushed it to the back of his head, in order to save his realm. \r\nWhile holding the Golden Apple above his head, Prislea shouted:\r\n";
             case 34: return "\"I wish for my realm to see nothing but safety and comfort for the rest of eternity!\"\r\n";
-            case 35: return "As the Golden Apple disappeared into an explosion of light, Praslea and the rest of the inhabitants of the realm of Men awaited their reward patiently, when suddenly a splitting headache launches all of them into grief.";
-            case 36: return "As Praslea could feel himself losing his grasp on reality, he realized what was truly happening:\r\n\"The Wizard…it’s his fault!\" The boy shouted, as he was dragged into a smoke cloud.\r\n";
+            case 35: return "As the Golden Apple disappeared into an explosion of light, Prislea and the rest of the inhabitants of the realm of Men awaited their reward patiently, when suddenly a splitting headache launches all of them into grief.";
+            case 36: return "As Prislea could feel himself losing his grasp on reality, he realized what was truly happening:\r\n\"The Wizard…it’s his fault!\" The boy shouted, as he was dragged into a smoke cloud.\r\n";
             case 37: return "In the smoke, The Crasnic now had hold over the inhabitants of the realm of Men, the Golden Apple was a lie created to lure in hopeless warriors:\r\n";
             case 38: return "\"Aha! Incredible, even I didn’t expect this to go so smoothly, all that’s left now is to conquer the Realm of Beasts, only then will I truly be unstoppable!\"\r\n";
             case 39: return "Back in the realm of Beasts, the warriors who had just witnessed this were shocked.";
@@ -156,6 +160,57 @@ public class StoryTellingManager : MonoBehaviour
                 storyText2.GetComponent<TextMeshProUGUI>().font = arhaicFont;
                 ChapterSelectManager.CH2 = true;
                 return " - End of Chapter 1 - ";
+            case 45:
+                storyText1.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "4th fight:";
+            case 46:
+                storyText2.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "Zmeul vs Evil Prislea\r\n";
+            case 47:
+                setFight(4);
+                return "";
+            case 48: return "";
+            case 49:
+                storyText1.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "Zmeul managed to get Prislea out of Crisnic's hypnosis.";
+            case 50: return "";
+            case 51:
+                storyText1.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "5th fight:";
+            case 52:
+                storyText2.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "Spinul vs Evil Harap-Alb\r\n";
+            case 53:
+                setFight(5);
+                return "";
+            case 54: return "";
+            case 55:
+                storyText1.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "Spinul helped Harap-Alb to escape the mind prison. \r\nNow Capcaunul is the last to take care of Greuceanu.";
+            case 56: return "";
+            case 57:
+                storyText1.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "6th fight:";
+            case 58:
+                storyText2.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "Capcaunul vs Evil Greuceanul\r\n";
+            case 59:
+                setFight(6);
+                return "";
+            case 60: return "";
+            case 61:
+                storyText1.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                return "Lastly Capcaunul did his job by waking Greuceanu up.";
+            case 62: return "";
+            case 63: return "As the mind-controlled warriors get defeated, The Crasnic lost control over them, and although the Realms of Men and Beasts are enemies, the warriors speak as such:\r\n";
+            case 64: return "\"Ugh, this guy, I think he’s trying to destroy all of us!\", said Prislea\r\nHarap-Alb continued: \"You don’t say! I can barely feel my legs! \"";
+            case 65: return "Suddenly, Spinul said:\r\n\"I know our realms don’t get along, but we have to team up to stop this guy, we’re all going to die if we don’t!\"";
+            case 66: return "Spinul’s words are interrupted by The Crasnic:";
+            case 67: return "\"If you don’t? You’ll die anyway! Zgripturoaica, Balaurul, finish them! Do not allow them to leave this place!\"";
+            case 68:
+                storyText2.GetComponent<TextMeshProUGUI>().font = arhaicFont;
+                ChapterSelectManager.CH3 = true;
+                return " - End of Chapter 2 - ";
             default:
                 return "N/A";
         }
@@ -179,6 +234,21 @@ public class StoryTellingManager : MonoBehaviour
         {
             ChoseCharacterManager.instance.characterChoosed(3);
             ChoseCharacterManager.instance.enemyChoosed(5);
+        }
+        else if (fightNb == 4)
+        {
+            ChoseCharacterManager.instance.characterChoosed(6);
+            ChoseCharacterManager.instance.enemyChoosed(1);
+        }
+        else if (fightNb == 5)
+        {
+            ChoseCharacterManager.instance.characterChoosed(4);
+            ChoseCharacterManager.instance.enemyChoosed(2);
+        }
+        else if (fightNb == 6)
+        {
+            ChoseCharacterManager.instance.characterChoosed(5);
+            ChoseCharacterManager.instance.enemyChoosed(3);
         }
 
         ChoseCharacterManager.instance.startGame();
